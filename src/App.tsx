895313490ecc192df1b800f7c9d63283f4b1a14c
Tesolite <pavel.kotka@gmail.com>
2026-03-25 17:52:20 +0000

    Project initialisation

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <button onClick={() => setCount(count + 1)}> Count is {count}</button>;
+}
+
+export default App;
